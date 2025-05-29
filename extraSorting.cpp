e3d9e7df690626b69fabf500d4c3036aa63fb4f3
Sparsh Rastogi <sparshh.sp26@gmail.com>
2025-05-29 13:28:50 +0530

    Added DNF sort

--- extraSorting.cpp
+++ extraSorting.cpp
@@ -22,8 +22,31 @@ void countSort(int limit, vector<int> &v){
     }
     return;
 }
+void DNFsort(vector<int> &v){
+    int ptr1=0;
+    int ptr2=0;
+    int ptr3 = v.size()-1;
+    while(ptr3>ptr2){
+        if(v[ptr2]==0){
+            swap(v[ptr1],v[ptr2]);
+            ptr2++;
+            ptr1++;
+        }
+        else if(v[ptr2]==1){
+            ptr2++;
+        }
+        else{
+            swap(v[ptr2],v[ptr3]);
+            ptr3--;
+        }
+    }
+    return;
+}
 int main(){
     vector<int> v = {1,5,5,4,3,4,5,2,1};
     countSort(5,v);
     printv(v);
+    vector<int> v2 = {1,2,0,0,2,1,0,1,2};
+    DNFsort(v2);
+    printv(v2);
 }
