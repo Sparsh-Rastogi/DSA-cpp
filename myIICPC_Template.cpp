d02efb9292a4a360b466ed4ccb39a9c41487b8fc
Sparsh Rastogi <sparshh.sp26@gmail.com>
2026-01-31 01:23:49 +0530

    added algos in template

--- myIICPC_Template.cpp
+++ myIICPC_Template.cpp
@@ -5,25 +5,150 @@ using namespace std;
 constexpr int MOD = 998244353;
 
 int qpow(int a, int x = MOD - 2) {
+    // if MOD is prime
+    //  qpow(a) = invModulo of a 
+    // (a^b)%(MOD) = ( a^( b%(MOD-1) ) )%MOD
     int res = 1;
     for (; x; x >>= 1, a = 1ll * a * a % MOD) if (x & 1) res = 1ll * res * a % MOD;
     return res;
 }
+vector<int> factt;
 
-vector<int> sieve(int n){
+int nCr(int n, int r){
+    int ans = factt[n];
+    ans %=MOD;
+    ans *= qpow(factt[r]);
+    ans %=MOD;
+    ans *= qpow(factt[n-r]);
+    ans %=MOD;
+    return ans;
+}
+
+vector<int> spf(int n){
     //LPrime is the lowest prime factor of the number
     vector<int> Lprime(n+1,1);
+    for(int i=1;i<=n;i++)Lprime[i]=i;
     for(int i=2;i<=n;i++){
-        if(Lprime[i]!=INT_MAX){continue;}
-        Lprime[i]=i;
-        for(int j=i*i;j<=n;j+=i){
-            Lprime[j] = min(Lprime[j],i);
+        if(Lprime[i]==i){
+            for(int j=i*i;j<=n;j+=i){
+                if(Lprime[j]==j){
+                    Lprime[j]=i;
+                }
+            }
+        }
+    }
+    return Lprime;
+}
+
+vector<int> sieve(int n){
+
+}
+
+struct SegTree {
+    int n;
+    vector<long long> tree, lazy;
+    
+    SegTree(int n) : n(n) {
+        tree.assign(4*n+5, 0); //identity element
+        lazy.assign(4*n+5, 0);
+    }
+
+    void build(int node, int l, int r, const vector<long long> &v) {
+        if (l == r) {
+            tree[node] = v[l];
+            return;
+        }
+        int mid = (l + r) >> 1;
+        build(node<<1, l, mid, v);
+        build(node<<1|1, mid+1, r, v);
+        tree[node] = tree[node<<1] + tree[node<<1|1]; //operation
+    }
+
+    inline void apply(int node, int l, int r, long long val) {
+        tree[node] += (r - l + 1) * val; //operation
+        if (l != r) {
+            lazy[node<<1] += val; //operation
+            lazy[node<<1|1] += val;
         }
     }
 
-    return Lprime;
-    
-}
+    void push(int node, int l, int r) {
+        if (lazy[node]) {
+            apply(node, l, r, lazy[node]);
+            lazy[node] = 0; //identity
+        }
+    }
+
+    void range_add(int node, int l, int r, int ql, int qr, long long val) {
+        push(node, l, r);
+        if (qr < l || r < ql) return;
+        if (ql <= l && r <= qr) {
+            apply(node, l, r, val);
+            return;
+        }
+        int mid = (l + r) >> 1;
+        range_add(node<<1, l, mid, ql, qr, val);
+        range_add(node<<1|1, mid+1, r, ql, qr, val);
+        tree[node] = tree[node<<1] + tree[node<<1|1]; //operation
+    }
+
+    long long range_sum(int node, int l, int r, int ql, int qr) {
+        push(node, l, r);
+        if (qr < l || r < ql) return 0; //identity
+        if (ql <= l && r <= qr) return tree[node];
+        int mid = (l + r) >> 1;
+        return range_sum(node<<1, l, mid, ql, qr)
+             + range_sum(node<<1|1, mid+1, r, ql, qr); //operation
+    }
+
+    void build(const vector<long long> &v) {
+        build(1, 1, n, v);
+    }
+
+    void range_add(int l, int r, long long val) {
+        range_add(1, 1, n, l, r, val);
+    }
+
+    long long range_sum(int l, int r) {
+        return range_sum(1, 1, n, l, r);
+    }
+};
+
+struct Fenwick {
+    int n;
+    vector<long long> bit; // 1-indexed
+
+    Fenwick(int n) : n(n), bit(n+1, 0) {}
+
+    // add val to index idx
+    void update(int idx, long long val) {
+        for (; idx <= n; idx += idx & -idx)
+            bit[idx] += val;
+    }
+
+    // sum from 1 to idx
+    long long query(int idx) const {
+        long long res = 0;
+        for (; idx > 0; idx -= idx & -idx)
+            res += bit[idx];
+        return res;
+    }
+
+    // sum from l to r
+    long long range_query(int l, int r) const {
+        return query(r) - query(l-1);
+    }
+
+    // build in O(n)
+    void build(const vector<long long> &a) {
+        for (int i = 1; i <= n; i++) {
+            bit[i] += a[i];
+            int j = i + (i & -i);
+            if (j <= n) bit[j] += bit[i];
+        }
+    }
+};
+
 long long sqrRoot(long long a, int isFloor=1){
     if(a==0){return a;}
     long long l = 1;
@@ -49,6 +174,47 @@ long long sqrRoot(long long a, int isFloor=1){
     return h+(isFloor&&(h*h!=a)?0:1);
 }
 
+long long merging(vector<int> &v, int start, int mid, int end){
+    vector <int> temp;
+    int i = start; int j = mid+1;
+    long long count_inv = 0;
+    while(i<=mid && j<=end){
+        if(v[i]<=v[j]){
+            temp.push_back(v[i]);
+            i++;
+        }
+        else{
+            temp.push_back(v[j]);
+            count_inv += mid-i+1;
+            // cout << i << " " << j << " " << count_inv << endl;
+            j++;
+        }
+    }
+    while(i<=mid){
+            temp.push_back(v[i]);
+            i++;
+    }
+    while(j<=end){
+        temp.push_back(v[j]);
+        j++;
+    }
+    for(int i = start;i<=end;i++){
+        v[i] = temp[i-start];
+    }
+    return count_inv;
+}
+long long recursive_merge(vector<int> &v, int start, int end){
+    long long count_inv = 0;
+    if(start<end){
+        int mid = start + (end-start)/2;
+        count_inv += recursive_merge(v,start,mid);
+        count_inv+= recursive_merge(v,mid+1,end);
+
+        count_inv += merging(v,start,mid,end);
+    }
+    return count_inv;
+}
+    
 vector<int> dijkstra(vector<vector<pair<int,int>>> &adjList){
     int n = adjList.size()-1;
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
